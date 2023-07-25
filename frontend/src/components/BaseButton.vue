ec62e408005c523e9515857a9cfaa86f45af900b
EFabrizioGC <fabriziocastellanos@hotmail.com>
2023-07-25 12:09:15 -0700

    Base Buttons :) and App Logo

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    as?: "a" | "button";
+  }>(),
+  {
+    as: "button",
+  }
+);
+</script>
+
+<template>
+  <Component
+    :is="as"
+    class="inline-flex items-center justify-center px-8 py-3 text-sm font-medium text-white transition"
+  >
+    <slot />
+  </Component>
+</template>
---
+++ frontend/src/components/ButtonPrimary.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseButton from "@/components/BaseButton.vue";
+</script>
+
+<template>
+  <BaseButton
+    class="bg-blue-800 text-white hover:bg-blue-700 disabled:bg-blue-500"
+  >
+    <slot />
+  </BaseButton>
+</template>
