3784621c7e0af5df3b4b22dd0ccfa2bc7ed7919d
EFabrizioGC <fabriziocastellanos@hotmail.com>
2023-07-25 12:59:12 -0700

    Add Pagination component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,14 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueAwesomePaginate);
 
 app.mount("#app");
